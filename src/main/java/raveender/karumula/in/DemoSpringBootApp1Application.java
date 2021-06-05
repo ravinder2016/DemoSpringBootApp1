188f447c2322260313923390f0365af60aaa2598
ravinder2016 <ravinder.nitw@gmail.com>
2021-06-05 15:07:03 +0530

    Initial Project Setup

---
+++ src/main/java/raveender/karumula/in/DemoSpringBootApp1Application.java
@@ -0,0 +1,13 @@
+package raveender.karumula.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/raveender/karumula/in/DemoSpringBootApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package raveender.karumula.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
